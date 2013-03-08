c315e52273bf788076399eed2742b1c2dbd96bed
sinourain <sinourain@gmail.com>
2013-03-08 16:30:15 -0500

    Add script Description, Author and Contributions

---
+++ rails_install_for_ubuntu_12.04_LTS.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# A simple Script for installing Rails on Ubuntu 12.04 LTS
+#
+# Author: Luis Mario Urrea Murillo <sinourain@gmail.com>
+#
+#
+# Contributions from: CEO. Miguel Diaz <gatoomega@gmail.com>
+# CODESCRUM S.A.S.
